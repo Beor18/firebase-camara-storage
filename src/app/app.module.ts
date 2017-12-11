174070f5989897f721555ebb41d3233857140069
Fernando Lopez <fer.ragnar19@gmail.com>
2017-12-11 15:46:59 -0300

    importamos modulos
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+//Importamos modulos
+import { Camera } from '@ionic-native/camera';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
