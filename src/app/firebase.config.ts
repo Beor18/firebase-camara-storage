98a5918c80123dbc5fd69146314b3ec42cbda469
Fernando Lopez <fer.ragnar19@gmail.com>
2017-12-11 15:47:16 -0300

    datos de firebase
    Signed-off-by: Fernando Lopez <fer.ragnar19@gmail.com>

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "TUS-DATOS",
+    authDomain: "TUS-DATOS",
+    databaseURL: "TUS-DATOS",
+    projectId: "TUS-DATOS",
+    storageBucket: "TUS-DATOS",
+    messagingSenderId: "TUS-DATOS"
+};
